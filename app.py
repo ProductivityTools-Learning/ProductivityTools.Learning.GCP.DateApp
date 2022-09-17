39879f9b5e2febbe5113e917f06d79b079284ab2
Pawel Wujczyk <pwujczyk@hotmail.com>
2022-09-17 21:33:45 +0200

    working version

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 from flask import Flask
 from flask_restful import Api
 from DateResource import DateResource
+import os
 
 def create_app():
     app=Flask(__name__)
     api=Api(app)
     api.add_resource(DateResource,'/')
+    return app
+
+if __name__ == "__main__":
+    app = create_app()
+    app.run(port=int(os.environ.get("PORT", 8080)), host='0.0.0.0', debug=True)
